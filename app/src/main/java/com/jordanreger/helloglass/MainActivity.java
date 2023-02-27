7da4a45df1bf531088615922db46ff3dafabafb9
Jordan Reger <mail@jordanreger.com>
2023-02-26 20:18:24 -0500

    refactor: rename for easier start

--- app/src/main/java/com/jordanreger/helloglass/MainActivity.java
+++ app/src/main/java/com/jordanreger/helloglass/MainActivity.java
@@ -3,10 +3,10 @@ package com.jordanreger.helloglass;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CardActivity extends Activity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.card);
+        setContentView(R.layout.activity_main);
     }
 }
